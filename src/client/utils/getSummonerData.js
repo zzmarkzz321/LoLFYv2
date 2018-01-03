890661563568a4c0cd9d3818495a2cb8448536d3
Mark renzo Santiago <ms103804@gmail.com>
2018-01-02 20:26:08 -0800

    start abstracting API calls in the client

--- src/client/utils/getSummonerData.js
+++ src/client/utils/getSummonerData.js
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 
+/**
+ * TODO: Abstract this function by creating an API wrapper and expose the direct get or post actions
+ */
 export const getSummonerData = summonerName => {
     return Axios.request({
         baseURL: 'https://us-central1-inspired-gift-162107.cloudfunctions.net/getSummonerMatchInfo',
@@ -11,66 +14,15 @@ export const getSummonerData = summonerName => {
 };
 
 /**
- * Function provided by: https://www.sitepoint.com/get-url-parameters-with-javascript/
- * Reformatted to comply with ES6
+ * TODO: use this function to get the Base URL rather than providing it directly in the Axios Call
  */
+const _getBaseUrl = () => {
+    // Complete this function
+};
 
-export const getUrlParams = url => {
-    // get query string from url (optional) or window
-    let queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-    // we'll store the parameters here
-    let obj = {};
-
-    // if query string exists
-    if (queryString) {
-
-        // stuff after # is not part of query string, so get rid of it
-        queryString = queryString.split('#')[0];
-
-        // split our query string into its component parts
-        let arr = queryString.split('&');
-
-        for (let i=0; i < arr.length; i++) {
-            // separate the keys and the values
-            let a = arr[i].split('=');
-
-            // in case params look like: list[]=thing1&list[]=thing2
-            let paramNum = undefined;
-            let paramName = a[0].replace(/\[\d*\]/, function(v) {
-                paramNum = v.slice(1,-1);
-                return '';
-            });
-
-            // set parameter value (use 'true' if empty)
-            let paramValue = typeof(a[1])==='undefined' ? true : a[1];
-
-            // (optional) keep case consistent
-            paramName = paramName.toLowerCase();
-            paramValue = paramValue.toLowerCase();
-
-            // if parameter name already exists
-            if (obj[paramName]) {
-                // convert value to array (if still string)
-                if (typeof obj[paramName] === 'string') {
-                    obj[paramName] = [obj[paramName]];
-                }
-                // if no array index number specified...
-                if (typeof paramNum === 'undefined') {
-                    // put the value on the end of the array
-                    obj[paramName].push(paramValue);
-                }
-                // if array index number specified...
-                else {
-                    // put the value at that index number
-                    obj[paramName][paramNum] = paramValue;
-                }
-            }
-            // if param name doesn't exist yet, set it
-            else {
-                obj[paramName] = paramValue;
-            }
-        }
-    }
-    return obj;
+/**
+ * TODO: export and invoke getSummonerData()
+ */
+const get = () => {
+    // Complete this function
 };
